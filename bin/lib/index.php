75a54912d26eb3e3a8f04c1263cac03b4562e1c1
Bayi <bayi@bayi.hu>
2025-07-14 20:29:21 +0200

    Fix .env loading, add vars to .env.example

--- bin/lib/index.php
+++ bin/lib/index.php
@@ -2,8 +2,10 @@
 define( 'BASEDIR', dirname( __FILE__ , 3));
 
 $env = [];
-if (file_exists(BASEDIR . '.env'))
-  $env = parse_ini_file(BASEDIR . '.env');
+if (file_exists(BASEDIR . '/.env'))
+{
+  $env = parse_ini_file(BASEDIR . '/.env');
+}
 
 include_once 'dictionary.php';
 include_once 'modrinth.php';
