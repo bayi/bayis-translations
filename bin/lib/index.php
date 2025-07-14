197f97d6db974f4b72cc5c7ac4836fe5f075735f
Bayi <bayi@bayi.hu>
2025-07-14 20:26:07 +0200

    add .env file loading

--- bin/lib/index.php
+++ bin/lib/index.php
@@ -1,5 +1,10 @@
 <?php
 define( 'BASEDIR', dirname( __FILE__ , 3));
+
+$env = [];
+if (file_exists(BASEDIR . '.env'))
+  $env = parse_ini_file(BASEDIR . '.env');
+
 include_once 'dictionary.php';
 include_once 'modrinth.php';
 include_once 'utils.php';
